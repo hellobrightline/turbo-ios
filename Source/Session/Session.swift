bdd4523bae056b52b28d54840212179aa5862390
Briar Bowser <bbowser@hellobrightline.com>
2025-01-22 16:31:58 -0600

    Backport WebNavigationDecision.

--- Source/Session/Session.swift
+++ Source/Session/Session.swift
@@ -348,7 +348,13 @@ extension Session: WebViewDelegate {
 
 extension Session: WKNavigationDelegate {
     public func webView(_ webView: WKWebView, decidePolicyFor navigationAction: WKNavigationAction, decisionHandler: @escaping (WKNavigationActionPolicy) -> ()) {
-        let navigationDecision = NavigationDecision(navigationAction: navigationAction)
+        let navigationDecision: WebNavigationDecision
+        if let decision = delegate?.session(self, webNavigationDecisionFor: navigationAction) {
+            navigationDecision = decision
+        } else {
+            log("delegate not set, the default decision policy for navigation will be used.")
+            navigationDecision = WebNavigationDecision.defaultDecision(for: navigationAction)
+        }
         decisionHandler(navigationDecision.policy)
 
         if let url = navigationDecision.externallyOpenableURL {
@@ -366,36 +372,6 @@ extension Session: WKNavigationDelegate {
     private func openExternalURL(_ url: URL) {
         log("openExternalURL", ["url": url])
         delegate?.session(self, openExternalURL: url)
-    }
-
-    private struct NavigationDecision {
-        let navigationAction: WKNavigationAction
-
-        var policy: WKNavigationActionPolicy {
-            navigationAction.navigationType == .linkActivated || isMainFrameNavigation ? .cancel : .allow
-        }
-
-        var externallyOpenableURL: URL? {
-            if let url = navigationAction.request.url, shouldOpenURLExternally {
-                return url
-            } else {
-                return nil
-            }
-        }
-
-        var shouldOpenURLExternally: Bool {
-            let type = navigationAction.navigationType
-            return type == .linkActivated || (isMainFrameNavigation && type == .other)
-        }
-
-        var shouldReloadPage: Bool {
-            let type = navigationAction.navigationType
-            return isMainFrameNavigation && type == .reload
-        }
-
-        var isMainFrameNavigation: Bool {
-            navigationAction.targetFrame?.isMainFrame ?? false
-        }
     }
 }
 
